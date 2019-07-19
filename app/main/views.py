313b78727cb0a8d0aaf75a3c803050140602e796
Danny Foo <tiangsin.foo@gmail.com>
2019-07-19 16:15:31 +0800

    select and text box understood!

--- app/main/views.py
+++ app/main/views.py
@@ -5,7 +5,13 @@ from .. import db
 from app.models import *
 
 @main.route('/')
-@main.route('/booking')
+@main.route('/home')
 def index():
     flights = Flight.query.all()
     return render_template('index.html', flights = flights)
+
+@main.route('/book', methods=['POST'])
+def book():
+    name = request.form.get('name')
+    flight = request.form.get('flight')
+    return render_template('hello.html', name=name, flight=flight)
